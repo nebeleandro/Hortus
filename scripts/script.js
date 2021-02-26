e3fd9be843170b1c2a44deddf18cfed1557bfdd3
Leandro Nebel <leandronebel@gmail.com>
2021-02-26 20:24:45 -0300

    detalle

--- scripts/script.js
+++ scripts/script.js
@@ -261,4 +261,10 @@ $('#infoClickCrot').slideToggle()
 $('#FotoCala').hover(function(){
 $('#infoClickCala').slideToggle()
 })
-    
+window.onload = function(){
+    document.getElementById('close').onclick = function(){
+        this.parentNode.parentNode.parentNode
+        .removeChild(this.parentNode.parentNode);
+        return false;
+    };
+};
