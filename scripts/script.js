05bc211e59291370115d7a5bacb1dd4a0313d8dc
Leandro Nebel <leandronebel@gmail.com>
2021-03-02 12:53:49 -0300

    limpieza js

--- scripts/script.js
+++ scripts/script.js
@@ -27,137 +27,32 @@ function tienenPlagas(){
 
 
 
-  /* var nroPlantas = parseInt( prompt ("Cuantas plantas tenés?"));
-    
-    if (nroPlantas > 0){
-        var tienen = tienenPlagas();
-   
-    }
-    else{
-        console.log("Deberías empezar a tener plantas en tu casa!")
-    }
-*/
-
-/*function Plaga (nombre, causas, efectos, solucionesOrganicas, solucionesQuimicas, dosificacionOrganica, dosificacionQuimica){
-    this.nombre = nombre;
-    this.causas = causas;
-    this.efectos = efectos;
-    this.solucionOrganica = solucionesOrganicas;
-    this.solucionesQuimicas = solucionesQuimicas;
-    this.dosificacionOrganica = dosificacionOrganica;
-    this.dosificacionQuimica = dosificacionQuimica;
-}
-
-let plaga = []
-
-plaga.push(new Plaga("cochinilla", "Estres o planta debil", "La planta no puede seguir desarrollandose y muere", "Jabón Potasico y/o Aceite neem", "Mamboreta D", "5ml/l","2ml/l"));
-plaga.push(new Plaga ("pulgon", "Exceso de nitrogeno", "Se alimenta de la salvia de la planta y esta no se puede desarrollar", "Jabón Potasico y/o Aceite neem", "Mamboreta D", "5ml/l","2ml/l"));
-plaga.push(new Plaga("hormigas", "Hormiguero cerca", "Come las hojas de las plantas, dependiendo el tamaño de la planta puede generar que no logre hacer la fotosíntesis y morirá", "Tierra de diatomea, preparado de ají picante o arroz molido", "Hortal", "Dos cucharas por litro de diatomea por litro de agua o polvorear al rededor de la planta, 5ml de solucion picante por litro","5ml/l"));
-plaga.push(new Plaga("araña roja", "altas temperaturas", "Se alimenta de celulas de la planta a una velocidad voraz, debilitandola y por ultimo matandola", "Jabón Potásico y/o Aceite de neem", "Mamboreta ABA", "5ml/l", "1ml/l"));
-
-
-plaga.forEach(el => console.log(el));
-
-/*
-for (let plaga of plaga) {
-    console.log(Plaga.nombre)
-}*/
-
-/*
-var cochinilla = new Plaga("Estres o planta debil", "La planta no puede seguir desarrollandose y muere", "Jabón Potasico y/o Aceite neem", "Mamboreta D", "5ml/l","2ml/l");
-
-var especificacion = prompt("Que información necesitas sobre la cochinilla?");
-console.log (cochinilla[especificacion.toLowerCase()]);
-while (!cochinilla[especificacion]){ console.log("Te podemos hablas sobre causas, efectos, solucionesOrganicas, solucionesQuimicas, dosificacionesOrganicas y dosificacionesQuimicas");
-console.log(cochinilla[especificacion]);
-especificacion = prompt ("Que información necesitas sobre la cochinilla?");
-console.log (cochinilla[especificacion.toLowerCase()]); } 
-//console.log (cochinilla);
-
-var pulgon = new Plaga("Exceso de nitrogeno", "Se alimenta de la salvia de la planta y esta no se puede desarrollar", "Jabón Potasico y/o Aceite neem", "Mamboreta D", "5ml/l","2ml/l");
-
-var especificacion = prompt("Que información necesitas sobre el pulgón?");
-console.log (pulgon[especificacion.toLowerCase()]);
-while (!pulgon[especificacion]){ console.log("Te podemos hablas sobre causas, efectos, solucionesOrganicas, solucionesQuimicas, dosificacionesOrganicas y dosificacionesQuimicas");
-console.log(pulgon[especificacion]);
-especificacion = prompt ("Que información necesitas sobre el pulgón?");
-console.log (pulgon[especificacion.toLowerCase()]); }  
-
-var hormiga = new Plaga("Hormiguero cerca", "Come las hojas de las plantas, dependiendo el tamaño de la planta puede generar que no logre hacer la fotosíntesis y morirá", "Tierra de diatomea, preparado de ají picante o arroz molido", "Hortal", "Dos cucharas por litro de diatomea por litro de agua o polvorear al rededor de la planta, 5ml de solucion picante por litro","5ml/l");
-
-var especificacion = prompt("Que información necesitas sobre la hormiga?");
-console.log (hormiga[especificacion.toLowerCase()]);
-
-while (!hormiga[especificacion]){ console.log("Te podemos hablas sobre causas, efectos, solucionesOrganicas, solucionesQuimicas, dosificacionesOrganicas y dosificacionesQuimicas");
-console.log(hormiga[especificacion]);
-especificacion = prompt ("Que información necesitas sobre la hormiga?");
-console.log (hormiga[especificacion.toLowerCase()]); 
-}*/
+ 
 function Info(cantPlantas, plaga){
     this.cantPlantas=cantPlantas;
     this.plaga=plaga;
 }
-
-/*function Plaga(nombre, causas, efectos, solucionOrganica, dosificacionOrganica){
-    this.nombre = nombre;
-    this.causas = causas;
-    this.efectos = efectos;
-    this.solucionOrganica = solucionOrganica;
-    this.dosificacionOrganica = dosificacionOrganica;
- }*/
-  //  this.dosificacionOrganica = function(){
-  //    return  parseInt(document.getElementById("inputPlaga").value) }
-
-    
-//function  tipoPlaga(Plaga){
- // return  Plaga = parseInt(document.getElementById("inputPlaga").value);
-    
-
-
-
-
-//function plantaXlitro(dosificacionOrganica){
-// return (dosificacionOrganica / 30);
-//}
-
-let plagas = []
-
-fetch('../scripts/const.json').then((res) => {
-    res.json().then((data) =>{
-        plagas = data
-        console.log(plagas)
-        })})
-
-
-/*const plagas=[
-    new Plaga("cochinilla", "Estres o planta debil", "La planta no puede seguir desarrollandose y muere", "Jabón Potasico y/o Aceite neem (5ml/l)", 5),
-    new Plaga("pulgon", "Exceso de nitrogeno", "Se alimenta de la salvia de la planta y esta no se puede desarrollar", "Jabón Potasico y/o Aceite neem (5ml/l)", 5),
-    new Plaga("hormigas", "Hormiguero cerca", "Come las hojas de las plantas, dependiendo el tamaño de la planta puede generar que no logre hacer la fotosíntesis y morirá", "Preparado de ají picante (5ml/l)", 5),
-    new Plaga("araña roja", "altas temperaturas", "Se alimenta de celulas de la planta a una velocidad voraz, debilitandola y por ultimo matandola", "Jabón Potásico y/o Aceite de neem (5ml/l)", 5),
-    
-]*/
 
 function calcularPrecio(event){
 
     console.log(event);
 
     // Pido los datos para el cálculo
-    //var cantPlantas= $("#inputPlantas").each((elemento) => {console.log(elemento.value)});
-    //var cantPlantas= $("#inputPlaga").each((elemento) => {console.log(elemento.value)});
+    
     var cantPlantas= parseInt(document.getElementById("inputPlantas").value)
     var tipo = (document.getElementById("inputPlaga").value)
 
     console.log(cantPlantas)
     console.log(tipo)
     console.log(plagas[tipo])
-   // console.log(dosiXPlantas(plagas[tipo]))
-    // Este es el cálculo del precio
+   
+    // Este es el cálculo de los litros
 
     var cantidadSv = cantPlantas / 30 + "l de preparado de agua más " + solucion(plagas[tipo]) + ". Las causas son: " + causa(plagas[tipo])
-  //  var preparado = parseInt(solucion(plagas[tipo]))
+ 
   
 
-    //var precio = $("#alert-calculo-precio");
+ 
     var precio = document.getElementById("alert-calculo-precio")
     precio.innerText = cantidadSv;
 
@@ -168,34 +63,14 @@ function calcularPrecio(event){
     return Number.parseFloat(cantidadSv).toFixed(2);
 }
 
-//function dosiXPlantas(plaga){
- //   return plaga.dosificacionOrganica / 150 ;
- // }
 
-  function solucion(plaga){
-      return plaga.solucionOrganica
-  }
+function solucion(plaga){
+    return plaga.solucionOrganica
+}
 
-  function causa(plaga){
-      return plaga.causas;
-  }
-
-
-$("#calcular").click(calcularPrecio);
-
-//var buttonCalcular = document.getElementById("calcular");
-
-//buttonCalcular.addEventListener("click", calcularPrecio);
-
-
-var cantPlantas = $("#inputPlantas")
-var plaga = $("#inputPlagas")
-//var cantPlantas = document.getElementById("inputPlantas")
-//var plaga = document.getElementById("inputPlagas")
-
-$("#inputPlantas").blur(validarCampo)
-//cantPlantas.addEventListener("blur", validarCampo);
-//plaga.addEventListener("blur", validarCampo);
+function causa(plaga){
+    return plaga.causas;
+}
 
 function validarCampo(event){
 
@@ -210,9 +85,25 @@ function validarCampo(event){
     }
 
 }
-//var info = $('#aromaticasClick');
 
-//info.remove();
+let plagas = []
+
+fetch('../scripts/const.json').then((res) => {
+    res.json().then((data) =>{
+        plagas = data
+        console.log(plagas)
+        })})
+
+
+var cantPlantas = $("#inputPlantas")
+var plaga = $("#inputPlagas")
+
+$("#calcular").click(calcularPrecio);
+
+
+$("#inputPlantas").blur(validarCampo)
+
+
 $('.btn-close').click(function(){
     window.location.reload();
 })
